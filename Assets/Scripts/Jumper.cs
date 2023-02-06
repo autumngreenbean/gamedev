7ef6b81dc7c861eca908be440f566081597a8159
unknown <dch@uoregon.edu>
2023-02-05 20:56:48 -0800

    Test Scene
    Made a test scene with some random objects and a script to make things jump.

---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Useless script example.
+/// </summary>
+public class Jumper : MonoBehaviour
+{
+    public float jumpSpeed = 1f;
+    public float jumpHeight = 1f;
+    public float jumpStartOffset = 1f;
+    public float timingOffset = 1f;
+
+    private bool jumping = false;
+    private bool reallyJumping = false;
+    private float startY;
+
+    void Start()
+    {
+        startY = transform.position.y;
+        StartCoroutine(StartJump());
+    }
+
+    public float ratio = 0f;
+
+    private void Update()
+    {
+        ratio = Mathf.PingPong((Time.time + timingOffset) * jumpSpeed, 1f);
+        if (jumping && ratio < 0.05f)
+            reallyJumping = true;
+
+        if (reallyJumping)
+            transform.position = new Vector3(
+                transform.position.x,
+                startY + jumpHeight * ratio,
+                transform.position.z
+            );
+    }
+
+    private IEnumerator StartJump()
+    {
+        yield return new WaitForSeconds(jumpStartOffset);
+        jumping = true;
+    }
+}
