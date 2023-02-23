a77a8b13153caecdf9d1df25fb34e86b7cd8239d
unknown <dch@uoregon.edu>
2023-02-22 22:48:35 -0800

    Globals class

---
+++ Assets/Scripts/General/Globals.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains all global methods and values.
+/// </summary>
+/// <remarks>
+/// All members must be static.
+/// </remarks>
+public static class Globals
+{
+    // Example
+    public static Vector3 gameOriginPosition = Vector3.zero;
+}
